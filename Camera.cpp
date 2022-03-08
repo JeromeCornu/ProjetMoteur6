00de6102865b73bb7e5f5648bf57141feaa97f6e
Jérôme <91902262+JeromeCornu@users.noreply.github.com>
2022-03-08 16:03:03 +0100

    triangles moche tournent

--- Camera.cpp
+++
@@ -1,7 +0,0 @@
-class Camera
-{
-	void
-
-	vec3 basis[3];
-	vec3 position
-};
--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@
 
 #include <SDL.h>
 #include <gl/GL.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <numeric>
 
 int main(int argc, char* argv[])
 {
@@ -23,27 +31,76 @@ int main(int argc, char* argv[])
     SDL_GLContext context = SDL_GL_CreateContext(win);
     SDL_GL_MakeCurrent(win, context);
 
-    bool apprunning = true;
-    while (apprunning)
+    auto prevTime = std::chrono::steady_clock::now();
+
+    bool appRunning = true;
+    while (appRunning)
     {
         SDL_Event curEvent;
         while (SDL_PollEvent(&curEvent))
         {
-
+            if (curEvent.type == SDL_QUIT)
+            {
+                appRunning = false;
+            }
         }
+
         glViewport(0, 0, 1024, 768);
         glClearColor(0.0, 0.0, 0.0, 0.0);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glColor4f(1.0, 1.0, 1.0, 1.0);
+
+
+        const float radius = 0.5;
+
+        auto curTime = std::chrono::steady_clock::now();
+        std::chrono::duration<float> fTime = curTime - prevTime;
+        float camX = glm::sin(fTime.count()) * radius;
+        float camZ = glm::cos(fTime.count()) * radius;
+
+        glm::vec3 cameraPos = glm::vec3(camX, 1.0, camZ);
+        glm::vec3 cameraTarget = glm::vec3(0.0, 0.0, 0.0);
+
+        // Creation de la camera
+        glm::mat4 view;
+        view = glm::lookAt(
+            cameraPos,                  //Position de la camera
+            cameraTarget,               //Cible à regarder
+            glm::vec3(0.0, 1.0, 0.0)    //position vertical
+        );
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadMatrixf(&view[0][0]);
+
+
+
+        // Test de profondeur
+        glEnable(GL_DEPTH_TEST);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDepthMask(GL_TRUE);
+        glDepthFunc(GL_LESS);
+
+
+        // Creation d'un triangle
+        glColor4f(0.0, 1.0, 0.0, 1.0);
         glBegin(GL_TRIANGLES);
         glVertex3f(-1.0, 0.0, 0.0);
         glVertex3f(0.0, 1.0, 0.0);
         glVertex3f(1.0, 0.0, 0.0);
-        
+
+        // Creation d'un autre triangle
+        glColor4f(1.0, 0.0, 0.0, 1.0);
+        glBegin(GL_TRIANGLES);
+        glVertex3f(0.0, 0.0, -1.0);
+        glVertex3f(0.0, 1.0, 0.0);
+        glVertex3f(0.0, 0.0, 1.0);
+
+
         glEnd();
 
         SDL_GL_SwapWindow(win);
+
+
+
 
     }
 
