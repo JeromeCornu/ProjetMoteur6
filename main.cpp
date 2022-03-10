8fd256988ec869f8c9af48c48c1a94d1cdc83405
BaptistErr <baptisteherr@aol.com>
2022-03-10 19:56:11 +0100

    c'est le bon

--- main.cpp
+++ main.cpp
@@ -6,6 +6,18 @@
 
 #include <SDL.h>
 #include <gl/GL.h>
+#include "dep/glm/glm/glm.hpp"
+#include "dep/glm/glm/ext.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <numeric>
+
+#include "gc_3d_defs.hpp"
+
+using namespace GC_3D;
 
 int main(int argc, char* argv[])
 {
@@ -23,27 +35,79 @@ int main(int argc, char* argv[])
     SDL_GLContext context = SDL_GL_CreateContext(win);
     SDL_GL_MakeCurrent(win, context);
 
-    bool apprunning = true;
-    while (apprunning)
+    auto prevTime = std::chrono::steady_clock::now();
+
+    glViewport(0, 0, 1024, 768);
+
+
+    bool appRunning = true;
+    while (appRunning)
     {
         SDL_Event curEvent;
+        int width, height;
+        SDL_GetWindowSize(win, &width, &height);
+
+        float ratio = width / (float)height;
+
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+
         while (SDL_PollEvent(&curEvent))
         {
+            if (curEvent.type == SDL_QUIT)
+            {
+                appRunning = false;
+            }
 
+            if (curEvent.window.type == SDL_WINDOWEVENT_RESIZED)
+            {
+                glViewport(0.0, 0.0, width, height);
+            }
         }
-        glViewport(0, 0, 1024, 768);
+
         glClearColor(0.0, 0.0, 0.0, 0.0);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glColor4f(1.0, 1.0, 1.0, 1.0);
-        glBegin(GL_TRIANGLES);
-        glVertex3f(-1.0, 0.0, 0.0);
-        glVertex3f(0.0, 1.0, 0.0);
-        glVertex3f(1.0, 0.0, 0.0);
-        
-        glEnd();
+
+
+        const float radius = 3.0;
+
+        auto curTime = std::chrono::steady_clock::now();
+        std::chrono::duration<float> fTime = curTime - prevTime;
+        float camX = glm::sin(fTime.count()) * radius;
+        float camZ = glm::cos(fTime.count()) * radius;
+
+        glm::vec3 cameraPos = glm::vec3(camX, 1.5, camZ);
+        glm::vec3 cameraTarget = glm::vec3(0.0, 0.0, 0.0);
+
+        // Creation de la camera
+        glm::mat4 view;
+        view = glm::lookAt(
+            cameraPos,                  //Position de la camera
+            cameraTarget,               //Cible Ã  regarder
+            glm::vec3(0.0, 1.0, 0.0)    //position vertical
+        );
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadMatrixf(&view[0][0]);
+
+        glm::mat4 projection = glm::perspective(70.0F * (3.1416F / 180.0F), ratio, 0.01F, 100.0F);
+        glMatrixMode(GL_PROJECTION);
+        glLoadMatrixf((float*)&projection);
+
+
+        // Test de profondeur
+        glEnable(GL_DEPTH_TEST);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDepthMask(GL_TRUE);
+        glDepthFunc(GL_LESS);
+
+
+        Geometry cube;
+        cube.Bind();
+        cube.Draw();
 
         SDL_GL_SwapWindow(win);
+
 
     }
 
