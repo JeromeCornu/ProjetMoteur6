4b765ebf7954b815e4f1a86d5140cd9556912b03
BaptistErr <baptisteherr@aol.com>
2022-03-11 12:18:09 +0100

    triangle persp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <windows.h>
 #endif
 
+#include "GL/glew.h"
 #include <SDL.h>
 #include <gl/GL.h>
 #include "dep/glm/glm/glm.hpp"
@@ -16,6 +17,7 @@
 #include <numeric>
 
 #include "gc_3d_defs.hpp"
+#include "loadShader.cpp"
 
 using namespace GC_3D;
 
@@ -35,22 +37,44 @@ int main(int argc, char* argv[])
     SDL_GLContext context = SDL_GL_CreateContext(win);
     SDL_GL_MakeCurrent(win, context);
 
+    glewInit();
+
     auto prevTime = std::chrono::steady_clock::now();
 
     glViewport(0, 0, 1024, 768);
 
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint programID = LoadShaders("C:/Users/bherr/Documents/projetMoteur/ProjetMoteur6/SimpleVertexShader.vertexshader", "C:/Users/bherr/Documents/projetMoteur/ProjetMoteur6/SimpleFragmentShader.fragmentshader");
+    glUseProgram(programID);
 
     bool appRunning = true;
     while (appRunning)
     {
         SDL_Event curEvent;
-        int width, height;
+        /*int width, height;
         SDL_GetWindowSize(win, &width, &height);
 
         float ratio = width / (float)height;
 
         glEnable(GL_LIGHTING);
-        glEnable(GL_LIGHT0);
+        glEnable(GL_LIGHT0);*/
 
         while (SDL_PollEvent(&curEvent))
         {
@@ -61,11 +85,55 @@ int main(int argc, char* argv[])
 
             if (curEvent.window.type == SDL_WINDOWEVENT_RESIZED)
             {
-                glViewport(0.0, 0.0, width, height);
+                //glViewport(0.0, 0.0, width, height);
             }
         }
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-        glClearColor(0.0, 0.0, 0.0, 0.0);
+        glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+
+        // Or, for an ortho camera :
+        //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+        // Camera matrix
+        glm::mat4 View = glm::lookAt(
+            glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+            glm::vec3(0, 0, 0), // and looks at the origin
+            glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+        );
+
+        // Model matrix : an identity matrix (model will be at the origin)
+        glm::mat4 Model = glm::mat4(1.0f);
+        glm::mat4 Move = glm::mat4(10.0f);
+        glm::mat4 Rotate = glm::mat4(20.0f);
+        glm::mat4 Resize = glm::mat4(20.0f);
+        Model = Resize * Rotate * Move * Model;
+        // Our ModelViewProjection : multiplication of our 3 matrices
+        glm::mat4 mvp = Projection * View * Model;
+
+        GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+        // Send our transformation to the currently bound shader, in the "MVP" uniform
+        // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
+
+        /*glClearColor(0.0, 0.0, 0.0, 0.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
 
@@ -104,13 +172,13 @@ int main(int argc, char* argv[])
 
         Geometry cube;
         cube.Bind();
-        cube.Draw();
+        cube.Draw();*/
 
         SDL_GL_SwapWindow(win);
 
 
     }
 
-    printf("Hello World");
+    //printf("Hello World");
     return 0;
 }
